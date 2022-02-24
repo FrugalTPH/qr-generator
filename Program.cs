94390e458c0ebf7d0ebbef679da1abd79a51af25
Thomas Hartley <tom@frugaldesign.co.uk>
2022-02-24 18:08:41 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace qr_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+				if (args.Length != 2) { return; }
+				new qrBuilder(args[0], args[1]).Execute();
+			}
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+			//string value = "Generation failed - it is likely that the valid command line paramenters were not passed!";
+			//try
+			//{
+			//	if (args.Length != 2) { return; }
+			//	new qrBuilder(args[0], args[1]).Execute();
+			//	value = "Generation Succeeded!";
+			//}
+			//finally
+			//{
+			//	#if !DEBUG
+			//		Console.WriteLine(value);
+			//		Console.WriteLine("Press any key to exit...");
+			//		Console.ReadKey();
+			//	#endif
+			//}
+			
+		}
+    }
+}
---
+++ qrBuilder.cs
@@ -0,0 +1,26 @@
+﻿using QRCoder;
+
+namespace qr_generator
+{
+    class qrBuilder
+    {
+
+		private const QRCodeGenerator.ECCLevel correctionLevel = QRCodeGenerator.ECCLevel.Q;
+		private const int resolution = 5;
+        private readonly string _content;
+		private readonly string _fileName;
+
+		public qrBuilder(string content, string fileName)
+		{
+			_content = content;
+			_fileName = fileName;
+		}
+
+		public void Execute()
+		{
+			var qrcode = new QRCode(new QRCodeGenerator().CreateQrCode(_content, correctionLevel, false, false, 0, -1));
+			qrcode.GetGraphic(resolution).Save(_fileName);
+		}
+
+	}
+}
